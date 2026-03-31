36a8925e903b7473d0942e014381c134ad7f6cf4
SirPojanMok <mohdfauzan0506@gmail.com>
2026-03-31 16:25:06 +0800

    Add FormType and Approver Model to the admin dashboard in forms/admin.py

--- Backend/forms/admin.py
+++ Backend/forms/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from forms.models.forms import FormType
+from forms.models.workflow import Approver
 # Register your models here.
+admin.site.register(FormType)
+admin.site.register(Approver)
