49c0b71f1b0a5f1310e8f9567a38f533c77345f5
SirPojanMok <mohdfauzan0506@gmail.com>
2026-03-31 16:20:01 +0800

    Add ApproverSet, ApprovalWorkflowSet, ApprovalStepSet in views/workflow.py

---
+++ Backend/forms/views/workflow.py
@@ -0,0 +1,38 @@
+from rest_framework import viewsets
+
+from forms.serializers.workflow import ApproverSerializer, ApprovalWorkflowSerializer, ApprovalStepSerializer
+from forms.models.workflow import Approver, ApprovalWorkflow, ApprovalStep
+
+class ApproverSet(viewsets.ModelViewSet):
+
+    queryset = Approver.objects.all()
+    serializer_class = ApproverSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(
+            created_by=self.request.user,
+            updated_by=self.request.user
+        )
+
+
+class ApprovalWorkflowSet(viewsets.ModelViewSet):
+
+    queryset = ApprovalWorkflow.objects.all()
+    serializer_class = ApprovalWorkflowSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(
+            created_by=self.request.user,
+            updated_by=self.request.user
+        )
+
+class ApprovalStepSet(viewsets.ModelViewSet):
+    
+    queryset = ApprovalStep.objects.all()
+    serializer_class = ApprovalStepSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(
+            created_by=self.request.user,
+            updated_by=self.request.user
+        )
