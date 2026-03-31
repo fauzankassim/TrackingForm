33f3bcdb38721558d913d1f2823e77ea6979c718
SirPojanMok <mohdfauzan0506@gmail.com>
2026-03-31 16:06:09 +0800

    Change HyperlinkModelSerializer to ModelSerializer in serializers/forms.py

--- Backend/forms/serializers/forms.py
+++ Backend/forms/serializers/forms.py
@@ -1,13 +1,13 @@
 from rest_framework import serializers
 from forms.models.forms import FormType, FormSchema
 
-class FormTypeSerializer(serializers.HyperlinkedModelSerializer):
+class FormTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = FormType
-        fields = ["url", "name", "is_active"]
+        fields = ["id", "name", "is_active"]
 
 
-class FormSchemaSerializer(serializers.HyperlinkedModelSerializer):
+class FormSchemaSerializer(serializers.ModelSerializer):
     class Meta:
         model = FormSchema
-        fields = ["url", "form_type", "schema", "version"]
+        fields = ["id", "form_type", "schema", "version"]
