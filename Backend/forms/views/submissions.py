e971ebeb5cda712c84094d1ec28882e38d564460
SirPojanMok <mohdfauzan0506@gmail.com>
2026-03-31 16:19:21 +0800

    Add FormSubmissionSet, FormSubmissionHistorySet, and FormSubmissionApprovalSet in views/submissions.py

---
+++ Backend/forms/views/submissions.py
@@ -0,0 +1,37 @@
+from rest_framework import viewsets
+
+from forms.serializers.submissions import FormSubmissionSerializer, FormSubmissionHistorySerializer, FormSubmissionApprovalSerializer
+from forms.models.submissions import FormSubmission, FormSubmissionHistory, FormSubmissionApproval
+
+class FormSubmissionSet(viewsets.ModelViewSet):
+
+    queryset = FormSubmission.objects.all()
+    serializer_class = FormSubmissionSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(
+            created_by=self.request.user,
+            updated_by=self.request.user
+        )
+
+class FormSubmissionHistorySet(viewsets.ModelViewSet):
+
+    queryset = FormSubmissionHistory.objects.all()
+    serializer_class = FormSubmissionHistorySerializer
+
+    def perform_create(self, serializer):
+        serializer.save(
+            created_by=self.request.user,
+            updated_by=self.request.user
+        )
+
+class FormSubmissionApprovalSet(viewsets.ModelViewSet):
+
+    queryset = FormSubmissionApproval.objects.all()
+    serializer_class = FormSubmissionApprovalSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(
+            created_by=self.request.user,
+            updated_by=self.request.user
+        )
