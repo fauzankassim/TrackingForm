3d9d838835030bd0afd029747c3e2e3e5f221d4a
SirPojanMok <mohdfauzan0506@gmail.com>
2026-03-31 16:18:34 +0800

    Add perform_create() method for FormTypeSet and FormSchemaSet in views/forms.py

--- Backend/forms/views/forms.py
+++ Backend/forms/views/forms.py
@@ -8,8 +8,20 @@ class FormTypeSet(viewsets.ModelViewSet):
     queryset = FormType.objects.all()
     serializer_class = FormTypeSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(
+            created_by=self.request.user,
+            updated_by=self.request.user
+        )
+
 
 class FormSchemaSet(viewsets.ModelViewSet):
 
     queryset = FormSchema.objects.all()
     serializer_class = FormSchemaSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(
+            created_by=self.request.user,
+            updated_by=self.request.user
+        )
