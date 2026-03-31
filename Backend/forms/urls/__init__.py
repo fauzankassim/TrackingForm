cf69c06b2a2c88ffb7849026aa9e155116e2db35
SirPojanMok <mohdfauzan0506@gmail.com>
2026-03-31 16:24:11 +0800

    Import urlpatterns from urls/forms, urls/submissions, and urls/workflow to concatenate it in urls/__init__.py

--- Backend/forms/urls/__init__.py
+++ Backend/forms/urls/__init__.py
@@ -1,1 +1,9 @@
-from .forms import *
+from .forms import urlpatterns as forms_urls
+from .submissions import urlpatterns as submissions_urls
+from .workflow import urlpatterns as workflow_urls
+
+urlpatterns = (
+    forms_urls 
+    + submissions_urls
+    + workflow_urls
+)
