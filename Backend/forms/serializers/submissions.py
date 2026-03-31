0e879285ed813cc6a70c86a4ec94d3cf77092f2e
SirPojanMok <mohdfauzan0506@gmail.com>
2026-03-31 16:07:32 +0800

    Add FormSubmissionSerializer, FormSubmissionHistorySerializer, and FormSubmissionApprovalSerializer in serializers/submissions.py

---
+++ Backend/forms/serializers/submissions.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from forms.models.submissions import FormSubmission, FormSubmissionHistory, FormSubmissionApproval
+
+class FormSubmissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FormSubmission
+        fields = ["id", "reference_number", "form_schema", "status", "pending_action_by"]
+
+
+class FormSubmissionHistorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FormSubmissionHistory
+        fields = ["id", "submission", "content", "version_number"]
+
+class FormSubmissionApprovalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FormSubmissionApproval
+        fields = ["id", "submission", "approval_step", "approver_group", "actioned_by", "status", "actioned_at", "comments"]
+
