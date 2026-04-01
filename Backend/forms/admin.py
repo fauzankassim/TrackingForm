bb1102e3fba16611c33c0e1386feefa7795be8d5
SirPojanMok <mohdfauzan0506@gmail.com>
2026-04-01 08:26:07 +0800

    Add FormSchema to the admin dasboard in forms/admin.py

--- Backend/forms/admin.py
+++ Backend/forms/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from forms.models.forms import FormType
+from forms.models.forms import FormType, FormSchema
 from forms.models.workflow import Approver
 # Register your models here.
 admin.site.register(FormType)
 admin.site.register(Approver)
+admin.site.register(FormSchema)
