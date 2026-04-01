d429a9bf5d1c6896caec7bb00cbb216779171253
SirPojanMok <mohdfauzan0506@gmail.com>
2026-04-01 13:32:52 +0800

    Add logic for version_number in FormSubmissionHistorySet in views/submissions.py

--- Backend/forms/views/submissions.py
+++ Backend/forms/views/submissions.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from django.db import transaction
+from django.db.models import Max
 
 from forms.serializers.submissions import FormSubmissionSerializer, FormSubmissionHistorySerializer, FormSubmissionApprovalSerializer
 from forms.models.submissions import FormSubmission, FormSubmissionHistory, FormSubmissionApproval
@@ -22,10 +24,23 @@ class FormSubmissionHistorySet(viewsets.ModelViewSet):
     http_method_names = ['post', 'get', 'delete', 'head', 'options']
     
     def perform_create(self, serializer):
-        serializer.save(
-            created_by=self.request.user,
-            updated_by=self.request.user
-        )
+        with transaction.atomic():
+            submission = serializer.validated_data.get("submission")
+
+            latest_version = (
+                FormSubmissionHistory.objects
+                .select_for_update()
+                .filter(submission=submission)
+                .aggregate(max_version=Max("version_number"))["max_version"]
+            )
+
+            next_version = (latest_version or 0) + 1
+
+            serializer.save(
+                version_number=next_version,
+                created_by=self.request.user,
+                updated_by=self.request.user
+            )
 
 class FormSubmissionApprovalSet(viewsets.ModelViewSet):
 
