ab6575badf6c7798d1ab8993a4e2eb0d6a43af53
SirPojanMok <mohdfauzan0506@gmail.com>
2026-04-01 08:20:36 +0800

    Add logic for schema versioning for FormSchemaSet in views/forms.py

--- Backend/forms/views/forms.py
+++ Backend/forms/views/forms.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from django.db import transaction
+from django.db.models import Max
 
 from forms.serializers.forms import FormTypeSerializer, FormSchemaSerializer
 from forms.models.forms import FormType, FormSchema
@@ -21,7 +23,20 @@ class FormSchemaSet(viewsets.ModelViewSet):
     serializer_class = FormSchemaSerializer
 
     def perform_create(self, serializer):
-        serializer.save(
-            created_by=self.request.user,
-            updated_by=self.request.user
-        )
+        with transaction.atomic():
+            form_type = serializer.validated_data.get("form_type")
+
+            latest_version = (
+                FormSchema.objects
+                .select_for_update()
+                .filter(form_type=form_type)
+                .aggregate(max_version=Max("version"))["max_version"]
+            )
+
+            next_version = (latest_version or 0) + 1
+
+            serializer.save(
+                version=next_version,
+                created_by=self.request.user,
+                updated_by=self.request.user
+            )
