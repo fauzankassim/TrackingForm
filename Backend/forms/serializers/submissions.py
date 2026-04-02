800ac8c35fa54eb31039ebb6b3ad080a74bd8693
SirPojanMok <mohdfauzan0506@gmail.com>
2026-04-02 09:31:24 +0800

    Change serializers.ModelSerializer to AuditBaseSerializer in serializers/submissions.py

--- Backend/forms/serializers/submissions.py
+++ Backend/forms/serializers/submissions.py
@@ -1,20 +1,20 @@
-from rest_framework import serializers
+from forms.serializers.audit import AuditBaseSerializer
 from forms.models.submissions import FormSubmission, FormSubmissionHistory, FormSubmissionApproval
 
-class FormSubmissionSerializer(serializers.ModelSerializer):
+class FormSubmissionSerializer(AuditBaseSerializer):
     class Meta:
         model = FormSubmission
         fields = ["id", "reference_number", "form_schema", "status", "pending_action_by"]
         read_only_fields = ["reference_number", "status", "pending_action_by"]
 
 
-class FormSubmissionHistorySerializer(serializers.ModelSerializer):
+class FormSubmissionHistorySerializer(AuditBaseSerializer):
     class Meta:
         model = FormSubmissionHistory
         fields = ["id", "submission", "content", "version_number"]
         read_only_fields = ["version_number"]
 
-class FormSubmissionApprovalSerializer(serializers.ModelSerializer):
+class FormSubmissionApprovalSerializer(AuditBaseSerializer):
     class Meta:
         model = FormSubmissionApproval
         fields = ["id", "submission", "approval_step", "approver_group", "actioned_by", "status", "actioned_at", "comments"]
