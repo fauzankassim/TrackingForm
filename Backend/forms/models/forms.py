6815f618e698d39a37ab22d1fc5784005034fca7
SirPojanMok <mohdfauzan0506@gmail.com>
2026-04-11 15:25:56 +0800

    Refactor: add slugfield to the FormType model in forms/models/forms.py for better referencing

--- Backend/forms/models/forms.py
+++ Backend/forms/models/forms.py
@@ -8,6 +8,12 @@ from django.utils.text import slugify
 class FormType(AuditBase):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
