944807dcd9d110a946790e3d49713c76d8be5e99
SirPojanMok <mohdfauzan0506@gmail.com>
2026-04-12 11:00:59 +0800

    Add rest_framework_simplejwt.token_blacklist to the INSTALLED_APPS in core/settings.py

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'rest_framework_simplejwt.token_blacklist',
     'forms.apps.FormsConfig',
     'accounts.apps.AccountsConfig'
 ]
