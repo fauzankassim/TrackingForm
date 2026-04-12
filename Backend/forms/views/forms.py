ee5a1876abe75445ee1817f6515e6abe18e5f89f
SirPojanMok <mohdfauzan0506@gmail.com>
2026-04-12 09:43:07 +0800

    Remove lookup_field and get_queryset() and list() logic of FormSchemaSet in forms/views/forms.py

--- Backend/forms/views/forms.py
+++ Backend/forms/views/forms.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from django.db import transaction
 from django.db.models import Max
-
+from rest_framework.response import Response
 from forms.serializers.forms import FormTypeSerializer, FormSchemaSerializer
 from forms.models.forms import FormType, FormSchema
 
@@ -20,8 +20,39 @@ class FormSchemaSet(viewsets.ModelViewSet):
     serializer_class = FormSchemaSerializer
     http_method_names = ['post', 'get', 'head', 'options']
     permission_classes = [IsAuthenticated]
-    lookup_field = "slug"
     
+    def get_queryset(self):
+        user = self.request.user
+        queryset = FormSchema.objects.all()
+        form_type = self.request.query_params.get("form_type")
+
+        if form_type:
+            queryset = (
+                queryset
+                .filter(form_type__slug = form_type)
+                .order_by("-version")[:1]
+            )
+            
+        else:
+            queryset = queryset.filter(created_by=user)
+        
+        return queryset
+    
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+
+        if "form_type" in request.query_params:
+            instance = queryset.first()
+            if not instance:
+                return Response(None)  # or {} or 404 depending on your design
+        
+            serializer = self.get_serializer(instance)
+            return Response(serializer.data)
+        
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+    
+
     def perform_create(self, serializer):
         with transaction.atomic():
             form_type = serializer.validated_data.get("form_type")
